d9347ea3c1382499680d488bfc6987536b27b667
bhwll <wang.liangliang@bohui.com.cn>
2017-10-25 10:33:28 +0800

    backend upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const koa = require('koa');
+
+const app = new koa();
+
+const http = require('http');
+
+const path = require('path');
+
+const config = require('./lib/config/config.js');
+
+const bodyParser = require('koa-bodyparser');
+
+const staticFiles = require('./lib/utils/static-file');
+
+const router = require('./lib/utils/router');
+
+const log4js = require('koa-log4');
+
+const server = http.createServer(app.callback());
+
+require('./lib/utils/log');
+
+const logger = log4js.getLogger('app') ;//将当前文件日志命名为 app
+logger.info('--------start project-------------');
+
+app.use(async (ctx, next) => {
+    logger.info(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    var
+      start = new Date().getTime(),
+      execTime;
+    await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
+  });
+
+app.use(staticFiles('/static',  path.join(__dirname, '/dist/static')));
+
+app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }));
+
+app.use(bodyParser());
+
+app.use(router());
+
+server.listen(config.localPort);
+
+logger.info('app started at port '+config.localPort+'...');
+
+
+
