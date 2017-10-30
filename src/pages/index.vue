10992314d7c97eb43cebe19447385a4c84539eee
Lost-Kun <2238632333@qq.com>
2017-10-31 01:21:03 +0800

    coding 2017.10.31 1:20

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -20,7 +20,7 @@
       </div>
       <div class="mainShowContainer_right">
         <div class="mainShowContainer_right_carouselPictureBox">
-          <el-carousel height="400px" :arrow="'never'" :indicator-position="'outside'">
+          <el-carousel height="400px" :arrow="'never'">
             <el-carousel-item v-for="(item, key) in carouselPictures" :key="'carouselPicture'+key">
               <img class="index_carouselPicture" :src="item" />
             </el-carousel-item>
@@ -28,7 +28,39 @@
         </div>
       </div>
     </div>
-    <div></div>
+    <div class="sentenceBlock">
+      为什么选择我们？
+    </div>
+    <div class="whyChoose">
+      <div class="whyChoose_item_box" v-for="(chooseItem, index) in whyChooseArr" :key="'choose'+index" >
+        <div :class="['whyChoose_item',chooseItem.isHover?'whyChoose_item--hover':'']" @mouseover="whyChooseMouseOver(chooseItem)" @mouseout="whyChooseMouseOut(chooseItem)">
+          <div class="whyChoose_item_icon">
+            <img :class="['whyChoose_item_icon_img',chooseItem.isHover?'whyChoose_item_icon_img--hover':'']" :src="chooseItem.src"/>
+          </div>
+          <div :class="['whyChoose_item_title',chooseItem.isHover?'whyChoose_item_title--hover':'']">
+            {{chooseItem.title}}
+          </div>
+          <div :class="['whyChoose_item_description',chooseItem.isHover?'whyChoose_item_description--hover':'']">
+            {{chooseItem.description}}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sentenceBlock">
+      他们这么说
+    </div> 
+    <div class="theySay">
+      <div class="theySay_box">
+          <el-carousel height="350px" :arrow="'never'" type="card" :autoplay="false" indicator-position="none">
+            <el-carousel-item v-for="(item, key) in carouselPictures" :key="'theySay'+key">
+              <img class="index_carouselPicture" style="width:500px;margin: 0 auto;" :src="item" />
+            </el-carousel-item>
+          </el-carousel>
+      </div>
+    </div>
+    <div class="sentenceBlock">
+      立即发布您的项目！
+    </div> 
   </div>
 </template>
 
@@ -42,7 +74,36 @@ export default {
         '../static/images/baima.jpg',
         '../static/images/huanxiang.jpg'
       ],
+      whyChooseArr:[
+        {
+          src:'../static/images/people.jpg',
+          title:'优质人才',
+          isHover:false,
+          description:'人才来自著名互联网公司，百度、腾讯、阿里、滴滴、搜狗、360、京东、美团等等，让您拥有一流的开发人员！'
+        },
+        {
+          src:'../static/images/rocket.jpg',
+          title:'快速对接',
+          isHover:false,
+          description:'企业提出用人需求后，客服一对一对接，全程参与筛选人才，不管是工作日还是双休日的需求，当天即可确定对口人才！'
+        },
+        {
+          src:'../static/images/money.jpg',
+          title:'资金担保',
+          isHover:false,
+          description:'创业公司将薪酬先预付到平台，待确认工程师完工后再实际支付，一小时内免费退款，保证项目顺利进展和资金安全！'
+        }
+      ]
     }
+  },
+  methods:{
+    whyChooseMouseOver(item){
+      item.isHover = true;
+    },
+    whyChooseMouseOut(item){
+      item.isHover = false;
+    }
+
   }
 }
 </script>
@@ -54,7 +115,7 @@ export default {
 
 .mainShowContainer{
   position: relative;
-  height: 640px;
+  height: 600px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -122,7 +183,7 @@ export default {
 .mainShowContainer_right{
   width: 600px;
   height: 500px;
-  margin-left: 20px;
+  margin-left: 10px;
 }
 
 }
@@ -166,6 +227,98 @@ export default {
   width: 100%;
   height: 100%;
 }
+
+.sentenceBlock{
+  height: 60px;
+  line-height: 60px;
+  font-size: 25px;
+}
+
+.whyChoose{
+  height: 380px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+  margin-bottom: 50px;
+}
+
+.whyChoose_item_box{
+  width: 30%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.whyChoose_item{
+  width: 350px;
+  height: 380px;
+  border: 1px solid #F5F5F5;
+}
+
+.whyChoose_item--hover{
+  /* border: 2px solid #F5F5F5; */
+  box-shadow: 0 4px 6px 0 rgba(0,0,0,.24), 0 0 8px 0 rgba(0,0,0,.08);
+}
+
+.whyChoose_item_icon{
+  height: 52%;
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.whyChoose_item_icon_img{
+  width: 150px;
+  height: 150px;
+}
+
+.whyChoose_item_icon_img--hover{
+  width: 160px;
+  height: 160px;
+}
+
+.whyChoose_item_title{
+  height: 15%;
+  width: 80%;
+  margin: 0 auto;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.whyChoose_item_title--hover{
+  font-size: 21px;
+}
+
+.whyChoose_item_description{
+  height: 30%;
+  width: 80%;
+  font-size: 16px;
+  margin: 0 auto;
+  line-height: 1.6;
+}
+
+.whyChoose_item_description--hover{
+  width: 85%;
+  font-size: 17px;
+}
+
+.theySay{
+  height: 350px;
+  margin-top: 30px;
+  margin-bottom: 60px;
+}
+
+.theySay_box{
+  width: 80%;
+  min-width: 1000px;
+  height: 100%;
+  margin: 0 auto;
+}
+
 </style>
 
 
