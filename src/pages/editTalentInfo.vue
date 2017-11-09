c5e5bc3a26880248943cd0da7a2fa8034f69032d
bhwll <wang.liangliang@bohui.com.cn>
2017-11-09 13:10:31 +0800

    coding 2017.11.9 13:10

--- src/pages/editTalentInfo.vue
+++ src/pages/editTalentInfo.vue
@@ -2,7 +2,9 @@
   <div class="editTalentInfo">
     <div class="editTalentInfo_item">
       <div class="editTalentInfo_item_title">
-
+        <div class="editTalentInfo_item_title_text">个人信息
+          <div class="editTalentInfo_item_title_text_tooltips">（私密信息，不公开展示）</div>
+        </div>
       </div>
       <div class="editTalentInfo_item_content">
 
@@ -23,6 +25,35 @@ export default {
 	min-width: 1100px;
 	margin: 0 auto;
 	width: 80%;
+}
+
+.editTalentInfo_item{
+  margin: 20px auto;
+}
+
+.editTalentInfo_item_title{
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.editTalentInfo_item_title_text{
+  position: relative;
+  height: 30px;
+  line-height: 30px;
+  font-size: 19px;
+  font-weight: bold;
+  color: #64A1D7;
+}
+
+.editTalentInfo_item_title_text_tooltips{
+    position: absolute;
+    top: 0;
+    left: calc(100% + 10px);
+    white-space: nowrap;
+    font-size: 15px;
+    color: red;
 }
 </style>
 
