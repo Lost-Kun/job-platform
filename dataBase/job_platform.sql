1b7b15cd4882f0ceedbea1833d96599ffd8b74e9
bhwll <wang.liangliang@bohui.com.cn>
2017-11-14 19:01:05 +0800

    初始数据库修改

--- dataBase/job_platform.sql
+++ dataBase/job_platform.sql
@@ -2,8 +2,7 @@
 SQLyog Ultimate v8.32 
 MySQL - 5.7.17-log : Database - job_platform
 *********************************************************************
-*/
-
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -22,7 +21,7 @@ USE `job_platform`;
 DROP TABLE IF EXISTS `apply`;
 
 CREATE TABLE `apply` (
-  `Apply_ID` bigint(20) NOT NULL COMMENT '每个申请的独特编号',
+  `Apply_ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '每个申请的独特编号',
   `Project_ID` bigint(20) NOT NULL COMMENT '项目编号',
   `Employee_ID` varchar(255) NOT NULL COMMENT '设计师编号',
   `Riqi` varchar(50) DEFAULT NULL COMMENT '提交这条log的具体时间，格式为年-月-日 时:分:秒',
@@ -37,6 +36,7 @@ DROP TABLE IF EXISTS `employee`;
 
 CREATE TABLE `employee` (
   `Employee_ID` varchar(255) NOT NULL COMMENT '设计师的唯一编号',
+  `HeadImgUrl` varchar(255) DEFAULT NULL COMMENT '头像地址',
   `Name` varchar(255) NOT NULL COMMENT '设计师昵称',
   `Name_real` varchar(255) DEFAULT NULL COMMENT '真实姓名',
   `Mobile` varchar(50) DEFAULT NULL COMMENT '手机号',
@@ -68,6 +68,7 @@ DROP TABLE IF EXISTS `employer`;
 
 CREATE TABLE `employer` (
   `Employer_ID` varchar(255) NOT NULL COMMENT '雇主的唯一编号',
+  `HeadImgUrl` varchar(255) DEFAULT NULL COMMENT '头像地址',
   `Name` varchar(255) NOT NULL COMMENT '雇主昵称',
   `Name_real` varchar(100) DEFAULT NULL COMMENT '真实姓名',
   `Mobile` varchar(20) DEFAULT NULL COMMENT '手机号',
@@ -98,7 +99,7 @@ CREATE TABLE `ip` (
 DROP TABLE IF EXISTS `log`;
 
 CREATE TABLE `log` (
-  `Log_ID` bigint(20) NOT NULL COMMENT '每条log记录的唯一编号',
+  `Log_ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '每条log记录的唯一编号',
   `Project_ID` bigint(20) NOT NULL COMMENT '项目编号',
   `Progress` varchar(1000) DEFAULT NULL COMMENT '这条log的详细内容',
   `Riqi` varchar(50) DEFAULT NULL COMMENT '提交这条log的具体时间，格式为年-月-日 时:分:秒',
@@ -112,12 +113,12 @@ CREATE TABLE `log` (
 DROP TABLE IF EXISTS `operating`;
 
 CREATE TABLE `operating` (
-  `Operation_ID` bigint(20) NOT NULL COMMENT '操作记录的唯一ID',
+  `Operation_ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '操作记录的唯一ID',
   `Employer_ID` varchar(255) DEFAULT NULL COMMENT '雇主和设计师的ID二选一',
   `Employee_ID` varchar(255) DEFAULT NULL COMMENT '雇主和设计师的ID二选一',
   `Ip_address` varchar(50) DEFAULT NULL COMMENT 'ip地址',
   `Page_url` varchar(255) DEFAULT NULL COMMENT '当前页面',
-  `Element` varchar(255) DEFAULT NULL COMMENT '操作对象，具体操作的html元素',
+  `Element` varchar(255) DEFAULT NULL COMMENT '操作对象，具体操作的html元素（具体可以讨论）',
   `Event` varchar(50) DEFAULT NULL COMMENT '操作类型，比如“点击”、“搜索”等等',
   `Desp` varchar(255) DEFAULT NULL COMMENT '这项操作的详情，比如操作是“搜索”，那么这里就记录搜索内容',
   `Riqi` varchar(50) DEFAULT NULL COMMENT '这项操作的具体日期',
@@ -131,7 +132,7 @@ CREATE TABLE `operating` (
 DROP TABLE IF EXISTS `project`;
 
 CREATE TABLE `project` (
-  `Project_ID` bigint(20) NOT NULL COMMENT '每个项目的唯一编号',
+  `Project_ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '每个项目的唯一编号',
   `Riqi` varchar(50) DEFAULT NULL COMMENT 'project的发布日期，格式为年-月-日 时:分:秒',
   `Name` varchar(255) DEFAULT NULL COMMENT '项目名称',
   `Desp` varchar(1000) DEFAULT NULL COMMENT '项目描述',
@@ -156,7 +157,7 @@ CREATE TABLE `project` (
 DROP TABLE IF EXISTS `rating`;
 
 CREATE TABLE `rating` (
-  `Rating_ID` bigint(20) NOT NULL COMMENT '每个评分的唯一编号',
+  `Rating_ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '每个评分的唯一编号',
   `Project_ID` bigint(20) NOT NULL COMMENT '项目编号',
   `Comment` varchar(1000) DEFAULT NULL COMMENT '评论（文本）',
   `Rating` float DEFAULT NULL COMMENT '评分（0-5）',
